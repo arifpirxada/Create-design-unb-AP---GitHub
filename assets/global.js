99ce222e49ba0c09e5c90e42942b8080fd368b9a
Arif Pirxada <arifpirxada@gmail.com>
2024-07-03 16:26:56 +0530

    Update - Animation complete - announcements complete

--- assets/global.js
+++ assets/global.js
@@ -1270,7 +1270,6 @@ if (!customElements.get('bulk-add')) {
 // Function for infinite slider =>
 
 const slide = () => {
-  console.log("running!")
   const scrollBar = document.querySelector("#scroll-bar");
 
   const announcementItems = Array.from(scrollBar.children)
